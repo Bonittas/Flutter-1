a16cb572d0f14cc5dd29164df8386db5c493477b
BIftu Shibbire <109244924+Bonittas@users.noreply.github.com>
2023-06-04 16:44:25 +0300

    Add files via upload

---
+++ flutter app/path.dart
@@ -0,0 +1,20 @@
+class Screen {
+  String splashScreen = '/';
+  String intro = '/intro';
+ 
+  String login = '/login';
+  String signup = '/signup';
+  String mainscreen = '/mainscreen';
+  
+  String search = "/search";
+  String adminScreen = "/adminScreen";
+
+  String inspectionDetail = '/inspectiondetail/:id';
+  String inspection = '/inspection';
+  String addInspection = "/addInspection";
+  String ListofInspection= "/ListofInspection";
+  
+  String admin = "/admin";
+  String adminList = "/adminList";
+  String profile = "/profile";
+}
